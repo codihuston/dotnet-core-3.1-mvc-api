519fe218edce5894374b248f537682e0c341f70d
Codi Huston (Home) <codi.huston540@gmail.com>
2020-09-29 09:56:52 -0500

    feat: implement api/commands, api/commands/:id
    - uses mock data

--- Commander/Controllers/CommandsController.cs
+++ Commander/Controllers/CommandsController.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using Commander.Data;
+using Commander.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Commander.Controllers
@@ -10,6 +13,23 @@ namespace Commander.Controllers
   [ApiController]
   public class CommandsController : ControllerBase
   {
-      
+    private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+
+    // GET api/commands
+    [HttpGet]
+    public ActionResult<IEnumerable<Command>> GetAllCommands()
+    {
+      var commandItems = _repository.GetAppCommands();
+
+      return Ok(commandItems);
+    }
+
+    // GET api/commands/{id}
+    [HttpGet("{id}")]
+    public ActionResult <Command> GetCommandById(int id)
+    {
+      var commandItem = _repository.GetCommandById(id);
+      return Ok(commandItem);
+    }
   }
 }
--- Commander/Data/MockCommanderRepo.cs
+++ Commander/Data/MockCommanderRepo.cs
@@ -9,7 +9,7 @@ namespace Commander.Data
     {
       var commands = new List<Command>
       {
-        new Command{Id=0, HowTo="Booil an egg", Line="Boil water", Platform="Kettle & Pan"},
+        new Command{Id=0, HowTo="Boil an egg", Line="Boil water", Platform="Kettle & Pan"},
         new Command{Id=0, HowTo="Cut Bread", Line="Get a knife", Platform="Knife & Chopping Board"},
         new Command{Id=0, HowTo="Make cup of te", Line="Place teabag in cup", Platform="Kettle & Cup"}
       };
@@ -19,7 +19,7 @@ namespace Commander.Data
 
     public Command GetCommandById(int id)
     {
-      return new Command{Id=0, HowTo="Booil an egg", Line="Boil water", Platform="Kettle & Pan"};
+      return new Command{Id=0, HowTo="Boil an egg", Line="Boil water", Platform="Kettle & Pan"};
     }
   }
 }
