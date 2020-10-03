9d27c48f01e0ab2989c3e92b11dd8d8f74648a00
Codi Huston (Home) <codi.huston540@gmail.com>
2020-10-03 13:29:48 -0500

    build: add entity framework (wip)

--- Commander/Controllers/CommandsController.cs
+++ Commander/Controllers/CommandsController.cs
@@ -13,14 +13,14 @@ namespace Commander.Controllers
   [ApiController]
   public class CommandsController : ControllerBase
   {
-    
+
     private readonly ICommanderRepo _repository;
 
     public CommandsController(ICommanderRepo repository)
     {
       _repository = repository;
     }
-
+    
     // GET api/commands
     [HttpGet]
     public ActionResult<IEnumerable<Command>> GetAllCommands()
